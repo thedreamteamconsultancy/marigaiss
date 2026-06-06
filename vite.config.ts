465ef639a9bc73f43a1fba604f799766a4ed3259
govardhan9692 <govardhan5009@gmail.com>
2026-06-06 15:46:28 +0530

    DS

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    nitro(),
     react(),
     tsconfigPaths(),
   ],
