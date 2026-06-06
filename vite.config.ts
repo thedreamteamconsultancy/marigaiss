bd2457477ae59327ceef929357d08a9c34c5a7e0
govardhan9692 <govardhan5009@gmail.com>
2026-06-06 15:38:20 +0530

    S

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
+    tanstackStart(),
     react(),
     tsconfigPaths(),
   ],
