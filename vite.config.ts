be3c0c38431a546db95955f881c5f25c67392a80
govardhan9692 <govardhan5009@gmail.com>
2026-06-06 15:51:25 +0530

    :S:

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { nitro } from "nitro/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
     nitro(),
+    tailwindcss(),
     react(),
     tsconfigPaths(),
   ],
